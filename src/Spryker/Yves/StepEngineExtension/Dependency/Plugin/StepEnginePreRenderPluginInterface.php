44a185052edee1b783d05ae3c3f23646953d2c86
David Greiner <hallo@davidgreiner.de>
2025-05-18 11:20:08 +0200

    refactor: replace misleading $quoteTransfer variable names with generic $abstractTransfer to reflect abstract transfer usage

--- src/Spryker/Yves/StepEngineExtension/Dependency/Plugin/StepEnginePreRenderPluginInterface.php
+++ src/Spryker/Yves/StepEngineExtension/Dependency/Plugin/StepEnginePreRenderPluginInterface.php
@@ -20,9 +20,9 @@ interface StepEnginePreRenderPluginInterface
      *
      * @api
      *
-     * @param \Generated\Shared\Transfer\QuoteTransfer $quoteTransfer
+     * @param \Generated\Shared\Transfer\QuoteTransfer $abstractTransfer
      *
      * @return \Generated\Shared\Transfer\QuoteTransfer
      */
-    public function execute(AbstractTransfer $quoteTransfer): AbstractTransfer;
+    public function execute(AbstractTransfer $abstractTransfer): AbstractTransfer;
 }
